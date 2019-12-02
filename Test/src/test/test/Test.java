4906f66ef5ea24c8c13aa4cc7f0f9b3a8bb00722
Sakura <Sakura@DESKTOP-UH2UEET>
2019-12-02 17:00:36 +0800

    master  分支提交

--- Test/src/test/test/Test.java
+++ Test/src/test/test/Test.java
@@ -3,5 +3,12 @@ package test.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("------------测试----------");
+		test();
 	}
+
+	private static void test() {
+		System.out.println("黑丝大概表格");
+	}
+	
+	
 }
