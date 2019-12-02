0758fc4055d5f5a04095a63c79b2e101e01a2b60
Sakura <Sakura@DESKTOP-UH2UEET>
2019-12-02 16:57:15 +0800

    分支测试   v2

--- Test/src/test/test/Test.java
+++ Test/src/test/test/Test.java
@@ -3,5 +3,8 @@ package test.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("------------测试----------");
+		if(Math.random()>0.5) {
+			System.out.println("就决定是你了");
+		}
 	}
 }
